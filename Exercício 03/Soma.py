5a1ad42d605fe89485539a053f7526afad52b889
Giovani Gomes <giovanigomes2002@gmail.com>
2021-12-05 22:58:10 -0300

    Exercício 03
    Resolução do Exercício 3 (Python).

---
+++ Exercício 03/Soma.py
@@ -0,0 +1,4 @@
+Numero1 = int(input("Digite um número : "))
+Numero2 = int (input("Digite Segundo número:"))
+Resultado = Numero1 + Numero2
+print('A soma entre {} e {} é igual á {}'.format(Numero1, Numero2, Resultado))
