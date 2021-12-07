48dd0c0f42b735822759e652d107064724527759
Giovani Gomes <giovanigomes2002@gmail.com>
2021-12-06 21:54:41 -0300

    Exercício 04
    Resolução do Exercício 04(Python).

---
+++ Exercício 04/media.py
@@ -0,0 +1,7 @@
+Nota1 = float(input("Digite Nota do Primeiro Bimestre: "))
+Nota2 = float(input("Digite Nora do Segundo Bimestre: "))
+Nota3 = float(input("Digite Nota do Terceiro Bimestre: "))
+Nota4 = float(input("Digite Nota do Quarto Bimestre: "))
+Media = (Nota1+Nota2+Nota3+Nota4)/4
+
+print("Sua Média é {0} ".format(Media))
