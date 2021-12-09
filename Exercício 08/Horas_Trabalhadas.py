bcb009e3a8b3d9223bab008bc49ce699da3c8046
Giovani Gomes <giovanigomes2002@gmail.com>
2021-12-08 22:55:00 -0300

    Exercício 08
    Resolução do Exercício 08 (Python)

---
+++ Exercício 08/Horas_Trabalhadas.py
@@ -0,0 +1,5 @@
+Ganho_Hora = float(input("Informe quanto ganha por Hora: "))
+Horas_Trabalhadas = int(input("Infome quantas horas você trablhou esse Mês: "))
+Total = Ganho_Hora * Horas_Trabalhadas
+
+print("Este Mês você ganhou : R${0}".format(Total))
