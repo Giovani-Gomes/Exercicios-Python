25573bd5de1566a0bbe38ebab54db2559ee7761a
Giovani Gomes <giovanigomes2002@gmail.com>
2021-12-16 00:01:13 -0300

    Update Exercício 10
    Corrigindo o Exercício 10 (Python)

--- Exercício 10/Celsius_Faremheit.py
+++ Exercício 10/Celsius_Faremheit.py
@@ -1,3 +1,3 @@
 Celsius = float(input("Informe a Temperatura em Celcius: "))
 Faremheit = (Celsius * 9/5) + 32
-print("A temperatura em Farenheit : {0} ".format(Faremheit))
+print("A temperatura em Farenheit : {0} °F ".format(Faremheit))
